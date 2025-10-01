b124a1139af4f4bbe1c818d21d339d0aae74a830
novyjpolzovatel <iskenova.nargiza@gmail.com>
2025-10-01 14:34:56 -0400

    corrected code and added overloaded constructor to the class.

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -7,6 +7,14 @@ public class CellPhone {
     private String phoneNumber;
     private String owner;
 
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+    }
+
     public CellPhone() {
         this.serialNumber = 0;
         this.model = "";
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -5,18 +5,29 @@ import java.util.Scanner;
 public class CellPhoneApplication {
     public static void main(String[] args) {
         CellPhone phone = new CellPhone();
+        CellPhone anotherPhone = new CellPhone(555555,"Nokia", "T-Mobile", "252-455-8632","Thomas");
+        // print short way
+        System.out.println(anotherPhone.getSerialNumber());
+
+        // print of variable stored
+        String output = anotherPhone.getModel();
+        System.out.println(output);
+
+        //printf
+        System.out.printf("%s %s %s", anotherPhone.getCarrier(), anotherPhone.getPhoneNumber(), anotherPhone.getOwner());
 
         Scanner userInput = new Scanner(System.in);
-        System.out.print("What is the serial number?");
+        System.out.println("What is the serial number?");
         int serialNum = userInput.nextInt();
         userInput.nextLine();
-        System.out.print("What model is the phone?");
+
+        System.out.println("What model is the phone?");
         String phoneModel = userInput.nextLine();
-        System.out.print("Who is the carrier?");
+        System.out.println("Who is the carrier?");
         String phoneCarrier = userInput.nextLine();
-        System.out.print("What is the phone number?");
+        System.out.println("What is the phone number?");
         String phoneNum = userInput.nextLine();
-        System.out.print("Who is the owner of the phone?");
+        System.out.println("Who is the owner of the phone?");
         String phoneOwner = userInput.nextLine();
 
         phone.setSerialNumber(serialNum);
@@ -33,6 +44,5 @@ public class CellPhoneApplication {
 
         phone.setOwner(phoneOwner);
         System.out.println(phone.getOwner());
-
     }
 }
